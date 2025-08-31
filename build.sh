f7a98e5993965d875333523ed70a6fcdf4af7a2d
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-31 01:54:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,14 @@ python manage.py collectstatic --noinput
 
 # Migraciones
 python manage.py migrate
+
+# Crear superusuario solo si no existe
+python - <<END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "Gabycobo12")
+    print("Superusuario creado ✅")
+else:
+    print("El superusuario ya existe ⚠️")
+END
