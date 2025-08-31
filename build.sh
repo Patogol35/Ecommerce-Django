aa2a2da3c47d76d782c0551844521ed2452eb241
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-31 01:58:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
+# Fail on errors
 set -o errexit
 
 # Instala dependencias
 pip install -r requirements.txt
 
-# Archivos estáticos
+# Recoge archivos estáticos
 python manage.py collectstatic --noinput
 
-# Migraciones
+# Aplica migraciones
 python manage.py migrate
 
 # Crear superusuario solo si no existe
 python - <<END
+import os
+import django
+
+# Ajusta esto al nombre de tu proyecto (el que contiene settings.py)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tiendaback.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "Gabycobo12")
+    User.objects.create_superuser("admin", "admin@example.com", "admin1234")
     print("Superusuario creado ✅")
 else:
     print("El superusuario ya existe ⚠️")
