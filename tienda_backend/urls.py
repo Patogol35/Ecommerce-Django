69d00d2e59704bd58e64402f3b0941a155bed5f8
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-31 13:25:10 -0500

    Update urls.py

--- tienda_backend/urls.py
+++ tienda_backend/urls.py
@@ -6,12 +6,13 @@ from rest_framework_simplejwt.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-# ✅ Servir archivos multimedia en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# ✅ Servir archivos multimedia SIEMPRE (Render no maneja media por defecto)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
