e4857b84c9faa758186616bf1dd04f2bf974ee9c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-20 15:03:05 -0500

    Update urls.py

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from .views import user_profile
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
