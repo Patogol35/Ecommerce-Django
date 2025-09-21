cc49364c8c1fdae2f37ad32e204ce3270ed7c1fd
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-20 23:15:01 -0500

    Update models.py

--- tienda/models.py
+++ tienda/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    imagen = models.URLField(max_length=500)
+    imagen = models.URLField(max_length=500)  # <-- cambiamos ImageField por URLField
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    categoria = models.CharField(max_length=100, null=True, blank=True)  # <- Permitir nulos
 
     def __str__(self):
         return self.nombre
