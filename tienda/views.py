6f9f573ee7bcdaaaf56ea5592485dd8a62486f3f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-21 09:50:03 -0500

    Update views.py

--- tienda/views.py
+++ tienda/views.py
@@ -6,15 +6,13 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-
-from .models import Producto, Carrito, ItemCarrito, Pedido, ItemPedido, Categoria
+from .models import Producto, Carrito, ItemCarrito, Pedido, ItemPedido
 from .serializers import (
     ProductoSerializer,
     CarritoSerializer,
     UserSerializer,
     ItemCarritoSerializer,
     PedidoSerializer,
-    CategoriaSerializer
 )
 
 # ---------------------------
@@ -119,14 +117,6 @@ class ProductoViewSet(viewsets.ModelViewSet):
 
 
 # ---------------------------
-# CRUD CATEGORÍAS
-# ---------------------------
-class CategoriaViewSet(viewsets.ModelViewSet):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
-
-
-# ---------------------------
 # CREAR PEDIDO
 # ---------------------------
 @api_view(['POST'])
@@ -177,9 +167,7 @@ class ListaPedidosUsuario(generics.ListAPIView):
         return Pedido.objects.filter(usuario=self.request.user).order_by('-fecha')
 
 
-# ---------------------------
-# PERFIL DE USUARIO
-# ---------------------------
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_profile(request):
