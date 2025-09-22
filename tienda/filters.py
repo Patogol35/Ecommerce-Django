38a39dfbe6c8e52ffcf3f2e848801363642723b3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-21 23:15:19 -0500

    Update filters.py

--- tienda/filters.py
+++ tienda/filters.py
@@ -5,6 +5,8 @@ class ProductoFilter(django_filters.FilterSet):
     precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
     stock_min = django_filters.NumberFilter(field_name='stock', lookup_expr='gte')
     stock_max = django_filters.NumberFilter(field_name='stock', lookup_expr='lte')
+    categoria = django_filters.CharFilter(field_name='categoria__nombre', lookup_expr='icontains')
+
     class Meta:
         model = Producto
-        fields = ['precio_min', 'precio_max', 'stock_min', 'stock_max']
+        fields = ['precio_min', 'precio_max', 'stock_min', 'stock_max', 'categoria']
