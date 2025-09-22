b5b92da2732a34cbe2beace72c700f3bc4bff56a
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-21 23:09:52 -0500

    Update models.py

--- tienda/models.py
+++ tienda/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    imagen = models.URLField(max_length=500)  # <-- cambiamos ImageField por URLField
+    imagen = models.URLField(max_length=500)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    # 🔹 Relación con categoría
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="productos"
+    )
 
     def __str__(self):
         return self.nombre
