72205b99589d447ea4c3ba8d201d379546588f22
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-21 20:54:18 -0500

    Update views.py

--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,4 @@
-Views.py from decimal import Decimal
+from decimal import Decimal
 from django.contrib.auth.models import User
 from django.db import transaction
 from rest_framework import viewsets, generics, permissions, status
