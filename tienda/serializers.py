6ee856eca9557005ebac804364d08af8ec247a9c
Patogol35 <patricio_jorge@hotmail.es>
2025-12-09 16:27:04 -0500

    cambios

--- tienda/serializers.py
+++ tienda/serializers.py
@@ -48,16 +48,20 @@ class CarritoSerializer(serializers.ModelSerializer):
 # USUARIO
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
-
+    # Email único
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Este correo ya está registrado.")
+        return value
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email'),
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
