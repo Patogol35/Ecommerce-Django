ad6c5a3b070ac907f910d8ed48d92ee9046d140a
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-16 22:32:54 -0500

    Update urls.py

--- tienda_backend/urls.py
+++ tienda_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from tienda.views import google_login  # 👈 NUEVO
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,9 +11,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+
+    # 👇 GOOGLE LOGIN
+    path('api/google-login/', google_login),
 ]
 
-# Servir archivos multimedia 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
