c8f24bbfa50c73d8556b7353d3d0bfa3bc81e2f3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-18 12:35:28 -0500

    Add stock filters to ProductoFilter

--- tienda/filters.py
+++ tienda/filters.py
@@ -5,12 +5,10 @@ from .models import Producto
 class ProductoFilter(django_filters.FilterSet):
     precio_min = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
     precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
-
+    stock_min = django_filters.NumberFilter(field_name='stock', lookup_expr='gte')
+    stock_max = django_filters.NumberFilter(field_name='stock', lookup_expr='lte')
     categoria = django_filters.NumberFilter(field_name='categoria__id')
-
-    # 🔥 FILTRO POR TALLA
-    talla = django_filters.CharFilter(field_name='variantes__talla')
 
     class Meta:
         model = Producto
-        fields = ['precio_min', 'precio_max', 'categoria', 'talla']
+        fields = ['precio_min', 'precio_max', 'stock_min', 'stock_max', 'categoria']
