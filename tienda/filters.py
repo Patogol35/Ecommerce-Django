7a073e50ea6bea5e1843c6697dd60c387ecadd43
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-18 10:21:25 -0500

    Update filters.py

--- tienda/filters.py
+++ tienda/filters.py
@@ -3,24 +3,12 @@ from .models import Producto
 
 
 class ProductoFilter(django_filters.FilterSet):
-    precio_min = django_filters.NumberFilter(method='filter_precio_min')
-    precio_max = django_filters.NumberFilter(method='filter_precio_max')
-    stock_min = django_filters.NumberFilter(method='filter_stock_min')
-    stock_max = django_filters.NumberFilter(method='filter_stock_max')
+    precio_min = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
+    precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
+    stock_min = django_filters.NumberFilter(field_name='stock', lookup_expr='gte')
+    stock_max = django_filters.NumberFilter(field_name='stock', lookup_expr='lte')
     categoria = django_filters.NumberFilter(field_name='categoria__id')
 
     class Meta:
         model = Producto
         fields = ['precio_min', 'precio_max', 'stock_min', 'stock_max', 'categoria']
-
-    def filter_precio_min(self, queryset, name, value):
-        return queryset.filter(variantes__precio__gte=value).distinct()
-
-    def filter_precio_max(self, queryset, name, value):
-        return queryset.filter(variantes__precio__lte=value).distinct()
-
-    def filter_stock_min(self, queryset, name, value):
-        return queryset.filter(variantes__stock__gte=value).distinct()
-
-    def filter_stock_max(self, queryset, name, value):
-        return queryset.filter(variantes__stock__lte=value).distinct()
